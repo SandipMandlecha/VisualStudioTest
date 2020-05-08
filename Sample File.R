3f30655d9592af4920311164576a4ade14157d0a
Sandip Mandlecha <sandip.mandlecha@coupa.com>
2020-05-08 11:39:48 +0530

    Sample Code Added

---
+++ Sample File.R
@@ -0,0 +1,8 @@
+setwd("C:/Users/Sandip Mandlecha/Documents/Sandip/Learning Material/Version Control/VisualStudioTest")
+
+library(data.table)
+
+mtCars = datasets::mtcars
+
+summary(mtCars)
+print(names(mtCars))
